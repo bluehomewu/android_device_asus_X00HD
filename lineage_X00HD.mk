29692b5e9e24c42e1aa92dacd23178ceef4b4107
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-04-23 12:18:03 +0200

    X00HD: inherit 64bit support

--- lineage_X00HD.mk
+++ lineage_X00HD.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit 64bit support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
